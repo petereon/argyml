f23e4f16f980d2f36157ff1d28744f58911f5e52
Peter Výboch <pvyboch1@gmail.com>
2023-07-05 19:55:39 +0200

    refactor: reordered and formatted

--- app/Main.hs
+++ app/Main.hs
@@ -1,54 +1,52 @@
 module Main where
-import System.Environment (getArgs)
-import Data.List
 
-data ArgStruct = ArgStruct { 
-    options :: [(String, String)],
+import Data.List
+import System.Environment (getArgs)
+
+data ArgLike = Option String String | Flag String | Arg String deriving (Show)
+
+data ArgStruct = ArgStruct
+  { options :: [(String, String)],
     flags :: [String],
     args :: [String]
-}
-
-quote :: String -> String
-quote s = "\"" ++ s ++ "\""
+  }
 
 instance Show ArgStruct where
   show (ArgStruct options' flags' args') =
     unlines $
-      ["options:"] ++
-      indentLines 2 (map (\(key, value) -> "- key: " ++ quote key ++ "\n    value: " ++ quote value) options') ++
-      ["flags:"] ++
-      indentLines 2 (map (\flag -> "- " ++ quote flag) flags') ++
-      ["args:"] ++
-      indentLines 2 (map (\arg -> "- " ++ quote arg) args')
+      ["options:"]
+        ++ indentLines 2 (map (\(key, value) -> "- key: " ++ quote key ++ "\n    value: " ++ quote value) options')
+        ++ ["flags:"]
+        ++ indentLines 2 (map (\flag -> "- " ++ quote flag) flags')
+        ++ ["args:"]
+        ++ indentLines 2 (map (\arg -> "- " ++ quote arg) args')
 
 indentLines :: Int -> [String] -> [String]
-indentLines indentSize = map (replicate indentSize ' ' ++) 
+indentLines indentSize = map (replicate indentSize ' ' ++)
 
-data ArgLike = Option String String | Flag String | Arg String deriving (Show)
+quote :: String -> String
+quote s = "\"" ++ s ++ "\""
 
 parseArgLike :: [String] -> [ArgLike]
 parseArgLike [] = []
 parseArgLike [x]
-    | "-" `isPrefixOf` x = [Flag x]
-    | otherwise = [Arg x]
-
-parseArgLike (x:y:xs) 
-    | "-" `isPrefixOf` x && "-" `isPrefixOf` y = Flag x : parseArgLike (y : xs)
-    | "-" `isPrefixOf` x = Option x y : parseArgLike xs
-    | otherwise = Arg x : parseArgLike (y : xs)
-
+  | "-" `isPrefixOf` x = [Flag x]
+  | otherwise = [Arg x]
+parseArgLike (x : y : xs)
+  | "-" `isPrefixOf` x && "-" `isPrefixOf` y = Flag x : parseArgLike (y : xs)
+  | "-" `isPrefixOf` x = Option x y : parseArgLike xs
+  | otherwise = Arg x : parseArgLike (y : xs)
 
 restructureArgLike :: [ArgLike] -> ArgStruct
 restructureArgLike [] = ArgStruct [] [] []
-restructureArgLike (x:xs) = case x of
-    Option a b -> ArgStruct ((a, b) : options rest) (flags rest) (args rest)
-    Flag a -> ArgStruct (options rest) (a : flags rest) (args rest)
-    Arg a -> ArgStruct (options rest) (flags rest) (a : args rest)
-    where rest = restructureArgLike xs
-
+restructureArgLike (x : xs) = case x of
+  Option a b -> ArgStruct ((a, b) : options rest) (flags rest) (args rest)
+  Flag a -> ArgStruct (options rest) (a : flags rest) (args rest)
+  Arg a -> ArgStruct (options rest) (flags rest) (a : args rest)
+  where
+    rest = restructureArgLike xs
 
 main :: IO ()
 main = do
-     arguments <- getArgs
-     print $ restructureArgLike $ parseArgLike arguments
-
+  arguments <- getArgs
+  print $ restructureArgLike $ parseArgLike arguments
